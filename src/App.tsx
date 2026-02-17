78a5791a9456f88191b35267a581bdbd735bbbf9
Ravi Kumar <141017178+ravikmr0@users.noreply.github.com>
2026-02-17 13:41:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+// import Hero from './components/Hero';
 import Programs from './components/Programs';
 import WhyChooseUs from './components/WhyChooseUs';
 import LearningExperience from './components/LearningExperience';
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <div className="min-h-screen bg-navy-950">
       <Navbar onEnrollClick={() => setEnrollModalOpen(true)} />
-      <Hero onEnrollClick={() => setEnrollModalOpen(true)} />
+      {/* <Hero onEnrollClick={() => setEnrollModalOpen(true)} /> */}
       <Programs />
       <WhyChooseUs />
       <LearningExperience />
